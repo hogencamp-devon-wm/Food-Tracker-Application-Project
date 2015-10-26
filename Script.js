d74d3fbb44e2e28240957336de72b1f3e3d10823
Devon Hogencamp <devon.hogencamp@west-mec.org>
2015-10-26 15:23:20 -0700

    Div/Style
    Set up basic divs and styles

--- Script.js
+++ Script.js
@@ -1,3 +1,13 @@
+// Variables
+
+var FoodName;
+
+var Description;
+
+var ShelfLife;
+
+// Jquery code
 $(document).ready(function(){
-    
+    var foodname = $("myForm").getElementsByName('foodName');
+    $("FoodTable").append(foodname);
 });
