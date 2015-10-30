3fe0601bea1d23425740e6388065878183e20780
Devon Hogencamp <devon.hogencamp@west-mec.org>
2015-10-30 15:27:31 -0700

    add

--- Script.js
+++ Script.js
@@ -1,11 +1,20 @@
 // Jquery code
 $(document).ready(function(){
     // Variables
+    
     var FoodName;
 
     var Description;
 
-    var ShelfLife;    
+    var ShelfLife;
+    
+    var Today = new Date();
+    
+    var CurrentDate = Date.parse(Today);
+    
+    var Experation;
+    
+    var FoodExperation = Date.parse(ShelfLife);
     
     $("#Submit").click(function() {
         
@@ -14,5 +23,18 @@ $(document).ready(function(){
         $("#Paragraph").html(ShelfLife);
         
     });
-    
+    if()
 });
+    
+    
+    
+ //Try this code   
+    
+/*
+
+var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+var firstDate = new Date(2008,01,12);
+var secondDate = new Date(2008,01,22);
+
+var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+*/
