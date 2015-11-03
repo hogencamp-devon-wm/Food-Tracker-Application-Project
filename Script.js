dfff93f56811d69eea6c6f7b3e3712fe92fdde33
Devon Hogencamp <devon.hogencamp@west-mec.org>
2015-11-02 17:59:04 -0700

    im done
    finally

--- Script.js
+++ Script.js
@@ -1,43 +1,15 @@
-$(document).ready(function() {
-  
-    $( "#startdate,#enddate" ).datepicker({
-        changeMonth: true,
-        changeYear: true,
-        firstDay: 1,
-        dateFormat: 'dd/mm/yy',
-    })
-
-    $( "#startdate" ).datepicker({ dateFormat: 'dd-mm-yy' });
-    $( "#enddate" ).datepicker({ dateFormat: 'dd-mm-yy' });
-
-    
-        var start = $('#startdate').datepicker('getDate');
-        var end   = $('#enddate').datepicker('getDate');
-
-        if (start<end) {
-            var days   = (end - start)/1000/60/60/24;
+function Output(){
+        var name =$('#Name').val();
+        var buy =$('#Start').val();
+        var bad =$('#End').val();
+        var desc =$('#description').val();
+        var unixExpiration = Date.parse(bad);
+        var today = new Date();
+        var unixToday = Date.parse(today);
+        if(unixToday > unixExpiration - 259200000){
+            $('#Output').append('<tr style="background-color: red"><td>' + name + '</td><td>' + buy + '</td><td> ' + bad + '</td><td>'+ desc + '</td></tr>');
         }
         else {
-            alert ("Your food is already expired, it is to late!");
-            $('#startdate').val("");
-            $('#enddate').val("");
-            $('#days').val("");
+            $('#Output').append('<tr><td>' + name + '</td><td>' + buy + '</td><td> ' + bad + '</td><td>'+ desc +'</td></tr>');
         }
-    
-
-    
-    
-    $("#Submit").click(function(){ 
-        var Name = $("#Name").val();
-        var Description = $("#Description").val();
-	   days = (end - start)/1000/60/60/24;
-        //If expired
-        if(days <= 3){
-		$("#Table").append('<tr style = "background-color: red"><td>' + Name + '</td><td>' + Description + '</td><td>' + days + '</td></tr>');   
-	   }
-	   //If not expired 
-	   else{
-		  $("#Table").append('<tr><td>' + Name + '</td><td>' + Description + '</td></tr>'); 
-	   } 
-    });
-}); //end ready
+    }
